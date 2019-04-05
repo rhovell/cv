60a8839ca36defb1c660371d4ee0cb54bfc86fed
Rachel Hovell <r.hovell@live.com>
2019-04-05 22:04:03 +0100

    Update

---
+++ java/app.js
@@ -0,0 +1,46 @@
+// show and hide the introduction
+$("#seeMoreIntro").click(function() {
+   var lable = $("#seeMoreIntro").text().trim();
+      if(lable == "^ See Less ^") {
+        $("#seeMoreIntro").text("v See More v");
+        $("#hideElements").hide();
+   }
+   else {
+     $("#seeMoreIntro").text("^ See Less ^");
+     $("#hideElements").show();
+   }
+  });
+var contactInfo = document.getElementById('contactInfo');
+var contact = document.getElementById('contact');
+contactInfo.onmouseover = function(){
+  contact.className = "contact show";
+}
+contactInfo.onmouseout= function(){
+  contact.className = "contact";
+}
+contact.onmouseover = function(){
+  contact.className = "contact show";
+}
+contact.onmouseout= function(){
+  contact.className = "contact";
+}
+
+// show and hide work exp
+$("#seeMoreWork").click(function() {
+   var lable = $("#seeMoreWork").text().trim();
+      if(lable == "^ See Less ^") {
+        $("#seeMoreWork").text("v See More v");
+        $("#hideWork").hide();
+   }
+   else {
+     $("#seeMoreWork").text("^ See Less ^");
+     $("#hideWork").show();
+   }
+  });
+
+// section selection
+// var meButton = document.getElementById('meButton');
+// var me = document.getElementById('me');
+// meButton.addEventListener('click', function(){
+//   me.scrollTo()
+// })
